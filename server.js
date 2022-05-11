66ebfe391e54159df91bc22fa90023df24eac73b
EC2 Default User <ec2-user@ip-172-31-22-233.ec2.internal>
2022-05-11 22:46:04 +0000

    pushing from ec2, fixed some origin and config stuff

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const refreshToken = process.env.REFRESH_TOKEN;
 
-console.log(refreshToken);
+console.log(process.env);
 
 const emailRouter = {
     ["ZANE"]: "zanecosmo@gmail.com",
@@ -47,19 +47,21 @@ const buildEmail = async (sender) => {
 };
 
 const corsOptions = {origin: "https://zanecosmo.com"};
+// const corsOptions = {origin: "http://127.0.0.1:5500"}
 
 app.options("/send-email", cors(corsOptions));
 app.use(bodyParser.json());
 
 app.post("/send-email", cors(corsOptions), (req, res) => {
-    buildEmail(req.body)
+       console.log("WE RECIEVED A REQUEST");
+       buildEmail(req.body)
         .then((result) => console.log(result))
         .catch((error) => console.log(error.message));
     res.send(req.body);
 });
 
 const server = http.createServer(app);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 server.listen(port, () => console.log(`LISTENING on PORT ${port}`));
 
